274c7df53cc10027f173ea8df4e548cd7955ac4c
Teodora-Georgieva <tgeorgieva15@yahoo.com>
2026-09-24 20:43:12 +0300

    Implemented api tests

--- src/test/java/tests/runners/TestRunner.java
+++ src/test/java/tests/runners/TestRunner.java
@@ -6,7 +6,6 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "tests",
-        tags = "@api",
         plugin = {"pretty"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
